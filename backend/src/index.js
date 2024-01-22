af1742dac2ea8f2e6832abc95cd311b372eceb2a
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-01-22 19:21:06 +0530

    create test api and route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js"
 import express from "express"
-
+import userRouter from "./routes/user.routes.js"
+//configure dotenv package
 dotenv.config({
     path:'./.env'
 })
 
-
-
+//connecting the Database with the backend
 connectDB()
 .then(()=>{
     console.log("connected to MongoDB")
@@ -16,7 +16,11 @@ connectDB()
     console.log(error)
 })
 
+//connection to our app
+
 const app = express()
 app.listen( process.env.PORT ||3000, ()=>{
     console.log(`listening on ${process.env.PORT}`)
 })
+
+app.use("/api/user",userRouter)
