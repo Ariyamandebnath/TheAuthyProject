28e426a4dbe4917b47fe3ad025801d6afa0cbc0a
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-01-23 12:26:43 +0530

    create signup api route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js"
 import express from "express"
-import userRouter from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 //configure dotenv package
 dotenv.config({
     path:'./.env'
@@ -19,8 +20,10 @@ connectDB()
 //connection to our app
 
 const app = express()
+app.use(express.json())
 app.listen( process.env.PORT ||3000, ()=>{
     console.log(`listening on ${process.env.PORT}`)
 })
 
-app.use("/api/user",userRouter)
+app.use("/api/user",userRoutes)
+app.use("/api/auth", authRoutes)
